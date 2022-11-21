f4501c41d3aeb9d2266bd1f9ab90567181159aa1
victor <victor.buthia@student.moringaschool.com>
2022-11-21 15:13:22 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+const currentUser = 'viktor';
+
+const welcomeMessage = `Welcome to Flatbook, currentUser' + ${currentUser}!`;
+
+const excitedWelcomeMessage =welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+
+
+
+
